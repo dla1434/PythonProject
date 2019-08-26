8f84e35b50c846a1aaec5ba2e02e10ccbbdc19c4
ljh <dla1434@naver.com>
2019-08-27 01:18:33 +0900

    19강 완료

---
+++ Python_Study_Alone/19_listAdd/01_list_append.py
@@ -0,0 +1,6 @@
+
+# list의 마지막에 추가하고 싶다면 append 메소드를 사용하면 된다.
+list = [1,2,3]
+list.append(4)
+
+print(list)
---
+++ Python_Study_Alone/19_listAdd/02_list_insert.py
@@ -0,0 +1,6 @@
+
+# list의 특정 위치에 추가를 하고 싶다면 insert를 사용한다.
+list = [1,3,4]
+list.insert(1, 2)
+
+print(list)
